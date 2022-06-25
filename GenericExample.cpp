82f53eb4d947fc0b26546a0230d42c2c8afc2500
huanmie <yonghenghuanmie@gmail.com>
2022-06-25 10:45:40 +0800

    Fix: IterateTupleElement's constructor

--- GenericExample.cpp
+++ GenericExample.cpp
@@ -1,5 +1,6 @@
 ﻿#include <array>
 #include <tuple>
+#include <string>
 #include <iostream>
 #include <iterator>
 #include <type_traits>
@@ -7,28 +8,31 @@
 class Dog
 {
 public:
-	Dog(int age) :age(age) {}
+	Dog(std::string name, int age) :name(std::move(name)), age(age) {}
 	void Bark() { std::cout << "汪汪" << std::endl; }
-	int GetAge() { return age; }
+	std::string GetName() { return name; }
 private:
+	std::string name;
 	int age;
 };
 
 class Cat
 {
 public:
-	Cat(int age) :age(age) {}
+	Cat(std::string name, int age) :name(std::move(name)), age(age) {}
 	void Bark() { std::cout << "喵喵" << std::endl; }
-	int GetAge() { return age; }
+	std::string GetName() { return name; }
 private:
+	std::string name;
 	int age;
 };
 
 template<typename Tuple>
+	requires requires(Tuple& tuple) { std::get<0>(tuple); }
 class IterateTupleElement
 {
 public:
-	IterateTupleElement(Tuple tuple) :tuple(tuple) {}
+	IterateTupleElement(Tuple& tuple) :tuple(tuple) {}
 
 	template<typename Callable, typename Result = std::invoke_result_t<Callable, decltype(std::get<0>(tuple))>,
 		bool is_void = std::is_same_v<void, Result>, typename Allow = std::enable_if_t<is_void> >
@@ -38,31 +42,7 @@ public:
 		map(callable, std::make_index_sequence<std::tuple_size_v<Tuple>>());
 	}
 
-	template<typename Callable, typename Result = std::invoke_result_t<Callable, decltype(std::get<0>(tuple))>,
-		bool is_void = std::is_same_v<void, Result>, typename Allow = std::enable_if_t<!is_void> >
-		requires requires(Callable callable, Tuple& tuple) { callable(std::get<0>(tuple)); }
-	auto map(Callable callable, void* overload = nullptr)
-	{
-		constexpr std::size_t size = std::tuple_size_v<Tuple>;
-		std::array<Result, size> results;
-		map(results, callable, std::make_index_sequence<size>());
-		return results;
-	}
-
 private:
-	template<typename Result, std::size_t Size, typename Callable, std::size_t Index, std::size_t... Rest>
-	auto map(std::array<Result, Size>& results, Callable callable, std::index_sequence<Index, Rest...>)
-	{
-		results[Index] = callable(std::get<Index>(tuple));
-		map(results, callable, std::index_sequence<Rest...>());
-	}
-
-	template<typename Result, std::size_t Size, typename Callable, std::size_t Index>
-	auto map(std::array<Result, Size>& results, Callable callable, std::index_sequence<Index>)
-	{
-		results[Index] = callable(std::get<Index>(tuple));
-	}
-
 	template<typename Callable, std::size_t Index, std::size_t... Rest>
 	void map(Callable callable, std::index_sequence<Index, Rest...>)
 	{
@@ -76,25 +56,51 @@ private:
 		callable(std::get<Index>(tuple));
 	}
 
+public:
+	template<typename Callable, typename Result = std::invoke_result_t<Callable, decltype(std::get<0>(tuple))>,
+		bool is_void = std::is_same_v<void, Result>, typename Allow = std::enable_if_t<!is_void> >
+		requires requires(Callable callable, Tuple& tuple) { callable(std::get<0>(tuple)); }
+	auto map(Callable callable, void* overload = nullptr)
+	{
+		constexpr std::size_t size = std::tuple_size_v<Tuple>;
+		std::array<Result, size> results;
+		map(results, callable, std::make_index_sequence<size>());
+		return results;
+	}
+
+private:
+	template<typename Result, std::size_t Size, typename Callable, std::size_t Index, std::size_t... Rest>
+	void map(std::array<Result, Size>& results, Callable callable, std::index_sequence<Index, Rest...>)
+	{
+		results[Index] = callable(std::get<Index>(tuple));
+		map(results, callable, std::index_sequence<Rest...>());
+	}
+
+	template<typename Result, std::size_t Size, typename Callable, std::size_t Index>
+	void map(std::array<Result, Size>& results, Callable callable, std::index_sequence<Index>)
+	{
+		results[Index] = callable(std::get<Index>(tuple));
+	}
+
 	Tuple& tuple;
 };
 
 
 int main()
 {
-	auto container = std::make_tuple<Dog>(2);
+	auto container = std::make_tuple<Dog>({ "alice", 2 });
 	{
 		auto& container_reference = container;
-		auto container = std::tuple_cat(container_reference, std::make_tuple<Cat>(3));
+		auto container = std::tuple_cat(std::move(container_reference), std::make_tuple<Cat>({ "bob", 3 }));
 		{
 			auto& container_reference = container;
-			auto container = std::tuple_cat(container_reference, std::make_tuple<Dog>(5));
+			auto container = std::tuple_cat(std::move(container_reference), std::make_tuple<Dog>({ "charlie", 5 }));
 
 			IterateTupleElement iterator(container);
 			iterator.map([](auto&& animal) {animal.Bark(); });
-			std::cout << "Ages: ";
-			for (auto&& age : iterator.map([](auto&& animal) {return animal.GetAge(); }))
-				*std::ostream_iterator<int>(std::cout, " ")++ = age;
+			std::cout << "Names: ";
+			for (auto&& name : iterator.map([](auto&& animal) {return animal.GetName(); }))
+				*std::ostream_iterator<std::string>(std::cout, " ")++ = name;
 		}
 	}
 	return 0;
